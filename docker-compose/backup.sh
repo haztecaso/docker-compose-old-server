104a75b2f53937c4a1883cd102539d64e72d2cbd
haztecaso <adrianlattes@disroot.org>
2021-10-23 17:52:14 +0200

    firefly config and backup script - moved to docker folder

---
+++ docker-compose/backup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+docker run -it --rm \
+    --name volumerize \
+    -v yourvolume:/source:ro \
+    -v backup_volume:/backup \
+    -v cache_volume:/volumerize-cache \
+    -e "VOLUMERIZE_SOURCE=/source" \
+    -e "VOLUMERIZE_TARGET=file:///backup" \
+    blacklabelops/volumerize backup
